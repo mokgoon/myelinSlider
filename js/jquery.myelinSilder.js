a78cdb41d1b229abadcc1de260ada2b9bedf83b3
mokgoon_x220 <mokgoon@gmail.com>
2015-05-26 16:52:33 +0900

    js refactoring

--- js/jquery.myelinSilder.js
+++ js/jquery.myelinSilder.js
@@ -9,27 +9,29 @@
 (function($){
 	
 	$.fn.myelinSlider = function(obj){
-		
-		
+
+		var firstOnBoolean, autoSlider, defaults, add, con;
+
 		function f() { 
 		   var args = [].slice.call( arguments, 1, 3); //배열의 slice() 메서드를 빌려 쓴다. 
 		   return args; 
-		} 
-		
-		function Elems(){};												// 객체 생성
-		var firstOnBoolean;												//	전역변수 생성
-		var autoSlider;
-		
+		}
+
+		// console.error
+		function errorFunction(msg){
+			console.error(msg);
+		}
+
 		// 기본 정의
-		var defaults = {
+		defaults = {
 			mode : 'fade',
 			item : 5,
 			direction : 'down',
 			auto : false,
 			speed : 3000
-		}
+		};
 		
-		var add = {
+		add = {
 			type : obj.type,
 			direction : obj.direction,
 			auto : obj.auto,
@@ -40,17 +42,21 @@
 		    item : this.children().eq(1).children().length,
 		    targetImg : this.children().eq(0).children().children(),
 		    targetTxt : this.children().eq(1).children().children()
-		}
+		};
 		
-		 var con = $.extend(defaults, add);
+		 con = $.extend(defaults, add);
 		
 				
 		// 슬라이드 이미지와, 텍스트의 갯수가 일치 하지않을때, 경고 창알림
-		if(con.dataImgLen !== con.item) errorFunction('배너이미지와 텍스트의 개수가 일치하지 않습니다.');
+		if(con.dataImgLen !== con.item){
+			errorFunction('배너이미지와 텍스트의 개수가 일치하지 않습니다.');
+		}
 		// 텍스트 첫번째에 on 클래스가 있는지 확인
 		firstOnBoolean = $(con.dataTxt).eq(0).hasClass('on');
 		// 없으면 추가해준다.
-		if(!firstOnBoolean) $(con.dataTxt).eq(0).addClass('on');
+		if(!firstOnBoolean){
+			$(con.dataTxt).eq(0).addClass('on');
+		 }
 		
 		// 텍스트에 hover 이벤트가 발생시 실행
 		$(con.dataTxt).hover(
@@ -58,7 +64,7 @@
 				con.auto = false;
 				clearInterval(autoSlider);
 				
-				if($(con.dataTxt).hasClass('on')) $(con.dataTxt).removeClass('on');
+				if($(con.dataTxt).hasClass('on')){$(con.dataTxt).removeClass('on');}
 				$(this).addClass('on');
 				var tagetNm = $(this).children()[0].className;
 				
@@ -98,11 +104,14 @@
 				}
 			}
 		}
-		
-		// console.error
-		function errorFunction(msg){
-			console.error(msg);
-		};
+
+		function getScreenHeight(){
+			var eleHeight = $('.banner').css('height');
+			$('.banner-img > li > a > img').css('height',eleHeight);
+		}
+
+		getScreenHeight();
+
 	}
 	
 })(jQuery);
